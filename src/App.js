83be4f8bc4d236079002a110f71dec5cb9b2f8e9
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-01-20 14:52:57 +0300

    Add a heart and shopping cart button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <header className="header">
-      <div className="container-fluid">
+      
         <div className="row">
           <div className="col-xs-3 col-lg-2">
             <div className="header__logo">
@@ -12,17 +12,34 @@ function App() {
           </div>
           <div className="col-xl-6 col-lg-7">
             <div className="input-group">
-              <input type="text" className="form-control">
-              </input>
-            <div className="input-group-append">
-              <button className="btn btn-light">
-                <i className="fa fa-search"></i>
-              </button>
-            </div>
+              <div className="input-group-prepend">
+                <select className="form-control btn btn-success dropdown-toggle">
+                  <option>ALL</option>
+                  <option>Men</option>
+                  <option>Women</option>
+                  <option>Kids</option>
+                </select>
+              </div>
+              <input type="text" className="form-control"></input>
+              <div className="input-group-append">
+                <button className="btn btn-success">
+                  <i className="fa fa-search"></i>
+                </button>
+              </div>
             </div>
           </div>
+          <div className="header__right">
+            <div className="auth">
+              <a href="#">SignIn</a>
+              <a href="#">SignUp</a>
+            </div>
+            <ul className="widget">
+              <li><i className="fa fa-heart"></i></li>
+              <li><i className="fa fa-shopping-cart"></i></li>
+            </ul>
+          </div>
         </div>
-      </div>
+      
     </header>
   );
 }
